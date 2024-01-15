6950a03364dd0f03a9315af6aa1f3c46817861f2
jeyeverett <jeysenfreedman@gmail.com>
2024-01-15 13:50:12 -0400

    comment out existing seed data

--- packages/server/prisma/seed.ts
+++ packages/server/prisma/seed.ts
@@ -2,39 +2,37 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 async function main() {
-  await prisma.user.upsert({
-    where: { email: 'jest-get-test-user@test.com' },
-    update: {},
-    create: {
-      id: 1,
-      email: 'jest-get-test-user@test.com',
-      passwordHash:
-        '$2b$12$4PwA75s4DQcC2pPKWjmZi.HAqh6hRn7NH7kIo5cUqkZgF9g3nmI4e',
-      accessToken: 'accessToken',
-    },
-  });
-
-  await prisma.user.upsert({
-    where: { email: 'jest-task-test-user@test.com' },
-    update: {},
-    create: {
-      id: 2,
-      email: 'jest-task-test-user@test.com',
-      passwordHash:
-        '$2b$12$4PwA75s4DQcC2pPKWjmZi.HAqh6hRn7NH7kIo5cUqkZgF9g3nmI4e',
-      accessToken: 'accessToken',
-    },
-  });
-
-  await prisma.task.upsert({
-    where: { id: 1 },
-    update: {},
-    create: {
-      id: 1,
-      type: 'other',
-      userId: 2,
-    },
-  });
+  // await prisma.user.upsert({
+  //   where: { email: 'jest-get-test-user@test.com' },
+  //   update: {},
+  //   create: {
+  //     id: 1,
+  //     email: 'jest-get-test-user@test.com',
+  //     passwordHash:
+  //       '$2b$12$4PwA75s4DQcC2pPKWjmZi.HAqh6hRn7NH7kIo5cUqkZgF9g3nmI4e',
+  //     accessToken: 'accessToken',
+  //   },
+  // });
+  // await prisma.user.upsert({
+  //   where: { email: 'jest-task-test-user@test.com' },
+  //   update: {},
+  //   create: {
+  //     id: 2,
+  //     email: 'jest-task-test-user@test.com',
+  //     passwordHash:
+  //       '$2b$12$4PwA75s4DQcC2pPKWjmZi.HAqh6hRn7NH7kIo5cUqkZgF9g3nmI4e',
+  //     accessToken: 'accessToken',
+  //   },
+  // });
+  // await prisma.task.upsert({
+  //   where: { id: 1 },
+  //   update: {},
+  //   create: {
+  //     id: 1,
+  //     type: 'other',
+  //     userId: 2,
+  //   },
+  // });
 }
 
 main()
